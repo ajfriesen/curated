f925810f127ded7b3dac3d04efad22b71033a939
Andrej Friesen <andre.friesen@gmail.com>
2022-04-05 21:28:28 +0200

    wip

--- self_hosted/views.py
+++ self_hosted/views.py
@@ -1,18 +1,15 @@
-from django.http import HttpResponse
-from django.template import loader
-
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import App
 
 def index(request):
     latest_app_list = App.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('self_hosted/index.html')
-    context = {
-        'latest_app_list': latest_app_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_app_list': latest_app_list}
+    return render(request, "self_hosted/index.html", context )
 
 
 def detail(request, app_id):
-    return HttpResponse("You're looking at app %s." % app_id)
+    app = get_object_or_404(App, pk=app_id)
+    return render(request, 'self_hosted/detail.html', {'app': app})
 
