416d042d7b998e50bef5475b37b845074e7f1a32
Andrej Friesen <andre.friesen@gmail.com>
2022-04-05 22:30:22 +0200

    wip

--- self_hosted/urls.py
+++ self_hosted/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'self-hosted'
 urlpatterns = [
     path('self-hosted', views.index, name='index'),
     path('self-hosted/<int:app_id>/', views.detail, name='detail'),
