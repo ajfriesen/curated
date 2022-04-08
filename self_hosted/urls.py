d6b1cf908c333c387b356ad7b8e1cf4b8304bbd8
Andrej Friesen <andre.friesen@gmail.com>
2022-04-08 10:09:33 +0200

    Add note about static files only beeing served in DEBUG mode

--- self_hosted/urls.py
+++ self_hosted/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'self-hosted'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/<int:app_id>/', views.detail, name='detail'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#TODO: Serving static and media files with django is not good for prod
+    path('<int:app_id>/', views.detail, name='detail'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Static files will only be served in DEBUG mode per default
+# https://docs.djangoproject.com/en/4.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
