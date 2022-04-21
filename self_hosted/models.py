cb1e73339892f717f92b5ea9dc0b9c4e8b8ab01f
Andrej Friesen <andre.friesen@gmail.com>
2022-04-22 01:15:44 +0200

    Change model

--- self_hosted/models.py
+++ self_hosted/models.py
@@ -13,7 +13,7 @@ def default_time():
 class App(models.Model):
     pub_date = models.DateTimeField('date published', default=default_time)
     app_name = models.CharField(max_length=250)
-    description = models.TextField(default="")
+    description = models.TextField(default="", blank=True)
     slug = AutoSlugField(populate_from=['app_name'])
     svg_logo = models.FileField(upload_to='logos', default="",blank=True)
 
