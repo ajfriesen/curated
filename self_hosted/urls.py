fdbb356af41a3901067a9a7f5b4234d76086e6d1
Andrej Friesen <andre.friesen@gmail.com>
2022-04-22 01:15:23 +0200

    Remove static files for local/debug setup

--- self_hosted/urls.py
+++ self_hosted/urls.py
@@ -8,6 +8,4 @@ app_name = 'self-hosted'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:app_id>/', views.detail, name='detail'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# Static files will only be served in DEBUG mode per default
-# https://docs.djangoproject.com/en/4.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+]
