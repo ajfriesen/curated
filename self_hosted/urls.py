0eb54e4ba8cefe0588c4621b9c2180408b9d7d5e
Andrej Friesen <andre.friesen@gmail.com>
2022-05-12 00:41:43 +0200

    Fix views

--- self_hosted/urls.py
+++ self_hosted/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from .views import AddAppView, index, detail
 
 app_name = 'self-hosted'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:app_id>/', views.detail, name='detail'),
+    path('', index, name='index'),
+    path('<int:app_id>/', detail, name='detail'),
+    path('create_app/', AddAppView.as_view() , name="add_app")
 ]
