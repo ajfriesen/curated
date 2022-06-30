4c68f11a3ab11c719d4d9cd89f307d7ade7985c7
ajfriesen <andre.friesen@gmail.com>
2022-06-30 16:58:32 +0200

    Add github_url field

--- self_hosted/models.py
+++ self_hosted/models.py
@@ -14,6 +14,7 @@ class App(models.Model):
     pub_date = models.DateTimeField('date published', default=default_time)
     app_name = models.CharField(max_length=250)
     description = models.TextField(default="", blank=True)
+    github_url = models.URLField(default="", blank=True, max_length=300)
     slug = AutoSlugField(populate_from=['app_name'])
     svg_logo = models.FileField(upload_to='logos/', default="",blank=True)
 
