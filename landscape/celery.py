b72df7ab073ceb3786b030e465412137f48e996e
ajfriesen <andre.friesen@gmail.com>
2022-06-30 21:19:04 +0200

    Cleanup celery.py

--- landscape/celery.py
+++ landscape/celery.py
@@ -1,7 +1,6 @@
-# from __future__ import absolute_import
-import os
 from __future__ import absolute_import, unicode_literals
-# from django.conf import settings
+from celery import Celery
+import os
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landscape.settings')
@@ -15,7 +14,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.task(bind=True, ignore_result=True)
 def debug_task(self):
