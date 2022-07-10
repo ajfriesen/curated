0ec99511ad7db575e54b571f15c6c1c166d2d9c7
ajfriesen <andre.friesen@gmail.com>
2022-07-10 22:50:25 +0200

    Use correct setting modul for working celery setup

--- curated/celery.py
+++ curated/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 import os
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'curated.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'curated.settings.local')
 app = Celery('curated')
 
 # Using a string here means the worker doesn't have to serialize
@@ -11,6 +11,12 @@ app = Celery('curated')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+# broker_url = 'redis://localhost:6379'
+
+# celery_broker_url = 'redis://localhost:6379/0'
+
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
